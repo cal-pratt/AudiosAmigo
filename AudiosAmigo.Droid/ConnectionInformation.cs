9b151c1b5252ef2362e418a2644b86e17aad1210
Cal Pratt <cpratt@mun.ca>
2016-07-15 02:51:56 -0230

    Update connection info dialog with active local server info

--- AudiosAmigo.Droid/ConnectionInformation.cs
+++ AudiosAmigo.Droid/ConnectionInformation.cs
@@ -1,43 +1,52 @@
 using System;
 using System.Collections.Generic;
+using System.Net;
+using System.Reactive.Concurrency;
 using System.Reactive.Linq;
 using System.Text.RegularExpressions;
 using Android.App;
-using Android.Content;
+using Android.Views;
 using Android.Widget;
 
 namespace AudiosAmigo.Droid
 {
     public class ConnectionInformation
     {
-        public static IObservable<Tuple<string, int>> FromDiaglog(Context context)
+        public static IObservable<Tuple<string, int>> FromDiaglog(Activity activity)
         {
-            var dialog = new Dialog(context);
+            var dialog = new Dialog(activity, Android.Resource.Style.ThemeHolo);
+            dialog.Window.SetSoftInputMode(SoftInput.AdjustPan);
             dialog.SetContentView(Resource.Layout.connect_menu);
             dialog.SetCancelable(false);
-            dialog.SetTitle("Enter Desktop Information");
+            dialog.SetTitle("    Enter Desktop Information");
 
             var ip = dialog.FindViewById<EditText>(Resource.Id.ip);
             var port = dialog.FindViewById<EditText>(Resource.Id.port);
             var password = dialog.FindViewById<EditText>(Resource.Id.password);
-            var button = dialog.FindViewById<Button>(Resource.Id.connect);
+            var connect = dialog.FindViewById<Button>(Resource.Id.connect);
+
+            var adapter = new ArrayAdapter(
+                activity, 
+                Android.Resource.Layout.SimpleListItem1,
+                new List<Java.Lang.String>());
+
             var ipListView = dialog.FindViewById<ListView>(Resource.Id.list);
-            var ips = new[]
-            {
-                "192.168.2.19:12345",
-                "111.222.333.444:55555",
-                "333.222.333.222:55555",
-                "111.222.333.444:55556",
-                "333.222.222.444:53335",
-                "333.222.333.333:55555",
-                "222.222.333.444:55556",
-                "333.222.333.444:55555",
-                "333.222.444.222:44488",
-                "111.222.333.444:55556"
-            };
-            var ipList = new List<string>(ips);
-            var adapter = new ArrayAdapter(context, Android.Resource.Layout.SimpleListItem1, ipList);
             ipListView.Adapter = adapter;
+
+            UdpUtil.ReceivePortInfo(Constants.ClientBroadcastListenerPort)
+                .ObserveOn(NewThreadScheduler.Default)
+                .SubscribeOn(NewThreadScheduler.Default)
+                .Subscribe(endpoint => activity.RunOnUiThread(() =>
+                {
+                    adapter.Add(new Java.Lang.String($"{endpoint.Address}:{endpoint.Port}"));
+                    adapter.NotifyDataSetChanged();
+                }));
+
+            NewThreadScheduler.Default.Schedule(() =>
+            {
+                UdpUtil.IntWriter(Constants.ClientBroadcastListenerPort).Invoke(
+                    new IPEndPoint(IPAddress.Broadcast, Constants.ServerBroadcastListenerPort));
+            });
 
             new ObservableOnItemClickListener<Java.Lang.String>(ipListView).Subscribe(item =>
             {
@@ -46,7 +55,7 @@ namespace AudiosAmigo.Droid
                 port.Text = split[1];
             });
 
-            var observableButton = new ObservableClickListener(button)
+            var observableButton = new ObservableClickListener(connect)
                 .Where(pressed => ip.Text != "" && Regex.IsMatch(port.Text, @"^\d+$"));
 
             dialog.Show();
