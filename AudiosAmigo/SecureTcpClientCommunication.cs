f8aed1781b2fd09a3c36ba273cc0c95c309b566a
Cal Pratt <cpratt@mun.ca>
2016-07-18 00:34:37 -0230

    Send encrypted data over a tcp connection

---
+++ AudiosAmigo/SecureTcpClientCommunication.cs
@@ -0,0 +1,42 @@
+﻿using System.Net.Sockets;
+
+namespace AudiosAmigo
+{
+    public class SecureTcpClientCommunication : INetworkCommunication
+    {
+        private readonly Encrpytion _encrpytion;
+
+        private readonly TcpClientCommunication _communication;
+
+        public SecureTcpClientCommunication(TcpClientCommunication communication, Encrpytion encrpytion)
+        {
+            _encrpytion = encrpytion;
+            _communication = communication;
+        }
+
+        public string Receive()
+        {
+            return Translate.ByteArrayToString(ReceiveBytes());
+        }
+
+        public byte[] ReceiveBytes()
+        {
+            return _encrpytion.Decrypt(_communication.ReceiveBytes());
+        }
+
+        public void Send(string text)
+        {
+            SendBytes(Translate.StringToByteArray(text));
+        }
+
+        public void SendBytes(byte[] buffer)
+        {
+            _communication.SendBytes(_encrpytion.Encrypt(buffer));
+        }
+
+        public void Close()
+        {
+            _communication.Close();
+        }
+    }
+}
