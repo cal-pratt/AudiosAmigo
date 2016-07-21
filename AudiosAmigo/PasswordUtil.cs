237d3c8c97cfef624f03614304c980af98536cb3
Cal Pratt <cpratt@mun.ca>
2016-07-21 21:07:37 -0230

    Generate a hash from a password

---
+++ AudiosAmigo/PasswordUtil.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Cryptography;
+
+namespace AudiosAmigo
+{
+    public static class PasswordUtil
+    {
+        public static byte[] PbkdfHash(byte[] password, byte[] salt, int iterations, int length)
+        {
+            using (var deriveBytes = new Rfc2898DeriveBytes(password, salt, iterations))
+            {
+                return deriveBytes.GetBytes(length);
+            }
+        }
+    }
+}
