bc49353e991ecaaa3aea524b537ead74431e6d91
Cal Pratt <cpratt@mun.ca>
2016-11-06 01:08:16 -0400

    Settings form for changing saved windows settings

---
+++ AudiosAmigo.Windows/SettingsForm.Designer.cs
@@ -0,0 +1,128 @@
+﻿using AudiosAmigo.Windows.Properties;
+
+namespace AudiosAmigo.Windows
+{
+    partial class SettingsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.portLabel = new System.Windows.Forms.Label();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.portText = new System.Windows.Forms.TextBox();
+            this.passwordText = new System.Windows.Forms.TextBox();
+            this.startupCheckBox = new System.Windows.Forms.CheckBox();
+            this.applyButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // portLabel
+            // 
+            this.portLabel.AutoSize = true;
+            this.portLabel.Location = new System.Drawing.Point(47, 15);
+            this.portLabel.Name = "portLabel";
+            this.portLabel.Size = new System.Drawing.Size(26, 13);
+            this.portLabel.TabIndex = 0;
+            this.portLabel.Text = "Port";
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Location = new System.Drawing.Point(20, 47);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(53, 13);
+            this.passwordLabel.TabIndex = 1;
+            this.passwordLabel.Text = "Password";
+            // 
+            // portText
+            // 
+            this.portText.Location = new System.Drawing.Point(79, 12);
+            this.portText.Name = "portText";
+            this.portText.Size = new System.Drawing.Size(100, 20);
+            this.portText.TabIndex = 2;
+            // 
+            // passwordText
+            // 
+            this.passwordText.Location = new System.Drawing.Point(79, 44);
+            this.passwordText.Name = "passwordText";
+            this.passwordText.PasswordChar = '*';
+            this.passwordText.Size = new System.Drawing.Size(100, 20);
+            this.passwordText.TabIndex = 3;
+            // 
+            // startupCheckBox
+            // 
+            this.startupCheckBox.AutoSize = true;
+            this.startupCheckBox.Location = new System.Drawing.Point(27, 79);
+            this.startupCheckBox.Name = "startupCheckBox";
+            this.startupCheckBox.Size = new System.Drawing.Size(116, 17);
+            this.startupCheckBox.TabIndex = 4;
+            this.startupCheckBox.Text = "Launch On Startup";
+            this.startupCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // applyButton
+            // 
+            this.applyButton.Location = new System.Drawing.Point(104, 102);
+            this.applyButton.Name = "applyButton";
+            this.applyButton.Size = new System.Drawing.Size(75, 23);
+            this.applyButton.TabIndex = 5;
+            this.applyButton.Text = "Apply";
+            this.applyButton.UseVisualStyleBackColor = true;
+            this.applyButton.Click += new System.EventHandler(this.applyButton_Click);
+            // 
+            // SettingsForm
+            // 
+            this.AcceptButton = this.applyButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(213, 132);
+            this.Controls.Add(this.applyButton);
+            this.Controls.Add(this.startupCheckBox);
+            this.Controls.Add(this.passwordText);
+            this.Controls.Add(this.portText);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.portLabel);
+            this.Icon = global::AudiosAmigo.Windows.Properties.Resources.AudioIcon;
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "SettingsForm";
+            this.ShowInTaskbar = false;
+            this.Text = "Audio\'s Amigo Settings";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label portLabel;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.TextBox portText;
+        private System.Windows.Forms.TextBox passwordText;
+        private System.Windows.Forms.CheckBox startupCheckBox;
+        private System.Windows.Forms.Button applyButton;
+    }
+}
